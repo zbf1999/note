d5b13d88fc8cf0b007ff18888ad93a3c404c8778
zbf1999 <zw949146236@outlook.com>
2020-06-23 23:57:03 +0800

    2020.6.23

--- java/hello.java
+++
@@ -1,7 +0,0 @@
-package note.java;
-
-public class hello {
-	public static void main(String args[]) throws Exception {
-		Runtime.getRuntime().exec("cmd /c start winword D:\\sc\\box\\zbf1999\\note\\java\\1.docx");
-	}
-}
